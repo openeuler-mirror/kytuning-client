8fdfc7035753b558bceb52795ce7c7383f47fa8c
liyl <liyulong@kylinos.cn>
2023-12-14 10:25:25 +0800

    update main

--- src/kytuning/main.py
+++ src/kytuning/main.py
@@ -24,11 +24,40 @@ class Main(object):
 
         with open(path, 'r') as f: 
             try:
+                scheme = SchemeParser().parse(f) 
+                scheme.prepare()
             except SchemeError as e:
+                print(e)
                 sys.exit()
             except SchemeParserError as e:
                 print(e)
                 sys.exit() 
+        try:
+            self.test = TestFactory().get_test_object(scheme.get_test_type(), scheme)
+            self.test.prepare() 
+        except TestNotFound as e:
+            logging.error(e)
+            sys.exit()
+        except SchemeError as e:
+            logging.error(e)
+            sys.exit()
+
+        try:
+            self.test.collect_env() 
+            self.test.install_dependent_rpms() 
+            self.test.setup_config(); 
+            self.test.do_test() 
+        except TestCaseError as e:
+            logging.error(e)
+        except Exception as e:
+            logging.error(e)
+        except BaseException as e:
+            logging.error(e)
+        finally: 
+            self.test.reset_config(); 
+            self.test.remove_dependent_rpms()
+            self.test.export()
+            logging.info('all tests are finshed.')
 
 if __name__ == '__main__':
     Main().run()
