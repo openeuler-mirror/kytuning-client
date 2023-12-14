4e15b61a2dde90cf44566704bb99bf4da3c5c70f
wangqingzheng <wangqingzheng@kylinos.cn>
2023-12-14 10:49:45 +0800

    增加dependency.py文件

---
+++ src/kytuning/dependency.py
@@ -0,0 +1,128 @@
+
+
+import os
+from .exec_cmd import ExecCmd
+
+class DependencyError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class DependencyManager(object):
+    env = dict(os.environ, LC_ALL="C")
+
+    def __init__(self, rpmlist = []):
+        self.rpmlist = rpmlist
+
+        # already instal, dont install again
+        self.dont_install = []
+
+        # not install, need  to install
+        self.need_install = []
+
+        # install success
+        self.succ_install = []
+
+        # install failed
+        self.fail_install = []
+
+        # uninstall success
+        self.succ_uninstall = []
+
+        # uninstall failed
+        self.fail_uninstall = []
+
+        if rpmlist == None:
+            return
+
+        for rpm in rpmlist:
+            installed = self.check_install(rpm)
+            if installed == True:
+                self.dont_install.append(rpm)
+            else:
+                self.need_install.append(rpm)
+
+
+    def install_once(self, rpm=None):
+        if rpm == None:
+            return False
+
+        cmd = 'yum -y install ' + rpm
+        result = ExecCmd(command = cmd, env = self.env).run()
+        return 0 if result.exit_status == 0 else 1
+
+
+    def install(self) -> list:
+        for rpm in self.need_install:
+           result =  self.install_once(rpm)
+           if result == 0:
+               self.succ_install.append(str(rpm))
+           else:
+               self.fail_install.append(str(rpm))
+
+        if len(self.fail_install) > 0:
+            # environment recovery
+            self.uninstall_norecord()
+            raise DependencyError("Dependency install failed:" + str(self.fail_install))
+
+        return self.succ_install
+
+
+    def uninstall_once(self, rpm):
+        if rpm == None:
+            return False
+
+        cmd = 'yum -y remove ' + rpm
+        result = ExecCmd(command = cmd, env = self.env).run()
+        return 0 if result.exit_status == 0 else 1
+
+
+    def uninstall_norecord(self):
+        for rpm in self.succ_install[::-1]:
+            result = self.uninstall_once(rpm)
+        
+
+    def uninstall(self) :
+        for rpm in self.succ_install[::-1]:
+            result = self.uninstall_once(rpm)
+            if result == 0:
+                self.succ_uninstall.append(str(rpm))
+            else:
+                self.fail_uninstall.append(str(rpm))
+                
+        if len(self.fail_uninstall) > 0:
+            raise DependencyError("Dependency uninstall failed:" + str(self.fail_uninstall))
+
+        return self.succ_uninstall
+
+
+    def check_install(self, rpm):
+        cmd = "rpm -qa  --queryformat '%{name}' " + rpm
+        result = ExecCmd(command = cmd, env = self.env).run()
+
+        '''
+        If the result.stdout is not empty, 
+        it means that the software package has been installed
+        '''
+        if result.exit_status == 0 and len(result.stdout) > 0:
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    dep = DependencyManager(['ctags', 'yelp', 'nano', 'notexist'])
+    try:
+        install = dep.install()
+    except Exception as e:
+        print(e.args)
+
+    uninstall = dep.uninstall()
+
+    print("dont install: ", dep.dont_install)
+    print("need install: ", dep.need_install)
+    print("succ install: ", dep.succ_install)
+    print("fail install: ", dep.fail_install)
+
+    print("succ uninstall: ", dep.succ_uninstall)
+    print("fail uninstall: ", dep.fail_uninstall)
