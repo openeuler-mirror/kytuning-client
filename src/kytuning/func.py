bb7645b2dab6b76de8819d76b9ac0ab6c964737e
liyl_kl <liyulong@kylinos.cn>
2024-02-23 15:55:43 +0800

    实现stream iozone配置文件处理接口

--- src/kytuning/func.py
+++ src/kytuning/func.py
@@ -24,12 +24,26 @@ class FUNC(object):
 
     # FUNC_THREAD_NUM
     def func_thread_num(self, type: str = None):
+        if type == 'single':
+            return '1'
+        elif type == 'multi':
+            result = ExecCmd(command = 'lscpu  | grep "^CPU(s)" | awk -F: \'{print $2}\'', env = self.curenv).run()
+            if result.exit_status == 0:
+                return result.stdout.strip()
         return None
 
     def func_iozone_memsize(self, type):
+        if type == 'half':
+            return KYConfig().get(['iozone', 'memsize', 'half'])
+        elif type == 'full':
+            return KYConfig().get(['iozone', 'memsize', 'full'])
+        elif type == 'double':
+            return KYConfig().get(['iozone', 'memsize', 'double'])
         return None
 
     def func_iozone_file(self, type):
+        if type is None or len(type) == 0:
+            return KYConfig().get(['iozone', 'test_file'])
         return None
 
     def func_jvm_mxmem(self, type):
