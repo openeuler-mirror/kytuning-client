31befaad41b13d515395e1ac9ee621310ecdcea8
ky_liyl <liyulong@kylinos.cn>
2024-11-27 11:01:20 +0800

    Console log output is supported

--- src/kytuning/logger.py
+++ src/kytuning/logger.py
@@ -17,7 +17,25 @@ def log_init(path='kytuning.log', levelname='info'):
     logging.addLevelName(logging.INFO, 'I')
     logging.addLevelName(logging.WARNING, 'W')
     logging.addLevelName(logging.ERROR, 'E')
-    logging.basicConfig(filename=path, level=level , format=Format, filemode='w')
+    #logging.basicConfig(filename=path, level=level , format=Format, filemode='w')
+
+    # 创建日志器
+    logger = logging.getLogger()
+    logger.setLevel(level)
+
+    # 文件日志处理器
+    file_handler = logging.FileHandler(path, mode='w')
+    file_handler.setLevel(level)
+    file_handler.setFormatter(logging.Formatter(Format))
+    
+    # 控制台日志处理器
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(level)
+    console_handler.setFormatter(logging.Formatter(Format))
+    
+    # 添加处理器到日志器
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
 if __name__ == '__main__':
 
